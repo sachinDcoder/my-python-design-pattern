2326499e73148c9a399ef99fc629c4779142589d
Raja Sachin <sachinra@vmware.com>
2024-06-30 01:12:32 +0530

    dependency inversion principle

--- solid-principles/dependency-inversion-principle/main-following-dip.py
+++ solid-principles/dependency-inversion-principle/main-following-dip.py
@@ -28,17 +28,17 @@ class Relationships(RelationshipBrowser):
             (parent, Relationship.PARENT, child)
         )
         self.relations.append(
-            (child, Relationship.PARENT, parent)
+            (child, Relationship.CHILD, parent)
         )
 
     def find_all_children_of(self, name):
-        return filter(lambda relation: relation[0].name == name and relation[1] == Relationship.PARENT, self.relations)
+        return list(filter(lambda relation: relation[0].name == name and relation[1] == Relationship.PARENT, self.relations))
 
 
 class Research:
     def __init__(self, browser, person):
         for p in browser.find_all_children_of(person.name):
-            print(f'{person.name} has a child called {p.name}')
+            print(f'{person.name} has a child called {p[2].name}')
 
 
 def main():
