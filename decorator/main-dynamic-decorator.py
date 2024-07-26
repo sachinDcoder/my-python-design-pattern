5b2c6fa696c73ec3d345c60efbec4158a9853b47
Raja Sachin <sachinra@vmware.com>
2024-07-26 19:43:19 +0530

    dynamic decorator

---
+++ decorator/main-dynamic-decorator.py
@@ -0,0 +1,38 @@
+class FileWithLogging:
+    def __init__(self, file):
+        self.file = file
+
+    def writelines(self, strings):
+        self.file.writelines(strings)
+        print(f'Wrote {len(strings)} lines')
+
+    def __iter__(self):
+        return self.file.__iter__()
+
+    def __next__(self):
+        return self.file.__next__()
+
+    def __getattr__(self, item):
+        return getattr(self.__dict__['file'], item)
+
+    def __setattr__(self, key, value):
+        if key == 'file':
+            self.__dict__[key] = value
+        else:
+            self.__dict__['file'] = key
+
+    def __delattr__(self, item):
+        delattr(self.__dict__['file'], item)
+
+
+def main():
+    file = FileWithLogging(open("hello.txt", 'w'))
+    file.writelines(["Hello", "World"])
+    file.write("Testing")
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
