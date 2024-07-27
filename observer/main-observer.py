752dc16ad0cc5324fae4c90159d92af038dca1a7
Raja Sachin <sachinra@vmware.com>
2024-07-28 00:44:30 +0530

    observer

---
+++ observer/main-observer.py
@@ -0,0 +1,35 @@
+class Event(list):
+    def __call__(self, *args, **kwargs):
+        for item in self:
+            item(*args, **kwargs)
+
+
+class Person:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.falls_ill = Event()
+
+    def catch_a_cold(self):
+        self.falls_ill(self.name, self.address)
+
+
+def call_doctor(name, address):
+    print(f'A doctor has been called to {address} for {name}')
+
+
+def main():
+    person = Person('Sherlock', 'Bangalore')
+    person.falls_ill.append(lambda name, address: print(f'{name} at {address} is ill'))
+    person.falls_ill.append(call_doctor)
+    person.catch_a_cold()
+
+    print()
+
+    person.falls_ill.remove(call_doctor)  # subscription removed
+    person.catch_a_cold()
+
+
+if __name__ == "__main__":
+    main()
+
